42bfd07fce2f5e67d590c272adc2693e836db58b
Christopher Goddard <cgoddard@zendesk.com>
2017-04-20 11:53:37 +1000

    add the bola
    add bola.rb

---
+++ lib/bola.rb
@@ -0,0 +1,10 @@
+class Hola::Bola
+  def self.hi
+    puts "Hallo mein freund"
+  end
+
+#  def roll_to_uncoil(weight, strength)
+#    dmg = weight*strength
+#    puts "You take " + dmg.to_s + " points of damage."
+#  end
+end
--- lib/hola.rb
+++ lib/hola.rb
@@ -2,4 +2,8 @@ class Hola
   def self.hi
     puts "Hello world!"
   end
+
+  def self.yes
+    puts '42'
+  end
 end
