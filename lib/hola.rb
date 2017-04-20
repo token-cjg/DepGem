e6a8b96e3e8e40efe36a52548686457f66b30761
Christopher Goddard <cgoddard@zendesk.com>
2017-04-20 15:07:10 +1000

    hola bola

--- lib/hola.rb
+++ lib/hola.rb
@@ -7,6 +7,12 @@ class Hola
   def self.yes
     puts '42'
   end
+
+  def self.bola(weight = 1, strength = 2)
+    bola = Bola.new(weight, strength)
+    bola.roll_to_uncoil
+  end
 end
 
+require 'hola/bola'
 require 'hola/translator'
--- lib/hola/bola.rb
+++ lib/hola/bola.rb
@@ -1,10 +1,15 @@
-class Hola::Bola
+class Bola
+  def initialize(w, s)
+    @weight = w
+    @strength = s
+  end
+
   def self.hi
     puts "Hallo mein freund"
   end
 
-#  def roll_to_uncoil(weight, strength)
-#    dmg = weight*strength
-#    puts "You take " + dmg.to_s + " points of damage."
-#  end
+  def roll_to_uncoil
+    dmg = @weight*@strength
+    puts "You take " + dmg.to_s + " points of damage."
+  end
 end
